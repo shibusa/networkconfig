564afa6282dddee2724932eb0ae24ab7538282f9
Shirfeng Chang <shirfeng.chang@gmail.com>
2017-02-16 23:59:13 -0800

    core cleanup, example core fix

--- core.py
+++ core.py
@@ -13,9 +13,3 @@ import vendor.juniper.returnstrings as JNPreturn
 # Menu item and functions
 # VARIABLE = MenuItem('MENU TEXT', {'VENDOR NAME': VENDORIMPORT.FUNCTION.run, 'VENDOR NAME 2': VENDORIMPORT2.FUNCTION.run})
 # menu.append(VARIABLE)
-interface = MenuItem("Assign IPv4 address to interface", {'juniper':JNPreturn.interface.run, 'cisco':CSCreturn.interface.run})
-menu.append(interface)
-vlanid = MenuItem("Create VLAN", {'juniper':JNPreturn.vlanid.run, 'cisco':CSCreturn.interface.run})
-menu.append(vlanid)
-vlantoint = MenuItem("Assign VLAN to interface", {'juniper':JNPreturn.vlantoint.run, 'cisco':CSCreturn.interface.run})
-menu.append(vlantoint)
