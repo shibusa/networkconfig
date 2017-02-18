24ee6ae64c311b32bdafbb3bf40ad95b6b6ad998
Shirfeng Chang <shirfeng.chang@gmail.com>
2017-02-17 23:50:22 -0800

    core default cleanup

--- core.py
+++ core.py
@@ -7,8 +7,6 @@ quit = MenuItem("Quit")
 
 # Import from vendors
 # import vendor.VENDORNAME.returnstrings as VENDORIMPORT
-import vendor.cisco.returnstrings as CSCreturn
-import vendor.juniper.returnstrings as JNPreturn
 
 # Menu item and functions
 # VARIABLE = MenuItem('MENU TEXT', {'VENDOR NAME': VENDORIMPORT.FUNCTION.run, 'VENDOR NAME 2': VENDORIMPORT2.FUNCTION.run})
